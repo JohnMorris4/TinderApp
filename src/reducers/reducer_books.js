4ba2fb7bf44af3dd5f0279f4cfc2dba381479aff
JohnMorris4 <johnmorris@morrisje.com>
2017-10-06 11:56:58 -0400

    Save after end of day

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+    return[
+        {title: 'Java'},
+        {title: 'Web Development'},
+        {title: 'How to cut your grass the wasy way'},
+        {title: 'Woodcraft for those with absolutly no experience'},
+        {title: 'Sharpen your tools'}
+    ]
+
+}
